35a9d4b23467732df0e93b601d666c3e32439f46
Danny Nhoisaykham <danny@MBP.local>
2017-09-16 21:22:55 -0500

    add bamazon customer

---
+++ app.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql');
+var faker = require('faker');
+
+// create mySql connection
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'bamazon'
+});
+
+// connect to database
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to database');
+
+  /*
+  // create database if it doesn't exist
+  connection.query('CREATE DATABASE IF NOT EXISTS bamazon', (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    console.log('Database created');
+  });
+  */
+
+  // create table if it doesn't exist
+  createTable();
+  seedData();
+});
+
+function createTable () {
+  var queryString = `CREATE TABLE IF NOT EXISTS products(
+    item_id INT(5) AUTO_INCREMENT,
+    product_name VARCHAR(100),
+    department_name VARCHAR(100),
+    price DECIMAL(10,2),
+    stock_quantity INT(10),
+    PRIMARY KEY(item_id)
+  )`;
+  connection.query(queryString, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    console.log('Created table');
+  });
+}
+
+function seedData () {
+  var item;
+  for (var i = 0; i < 10; i++) {
+    var productName = faker.commerce.productName();
+    var department = faker.commerce.department();
+    var price = faker.commerce.price();
+    var quantity = faker.random.number();
+    item = [productName, department, price, quantity];
+    insertNewProduct(item);
+  }
+}
+
+function insertNewProduct (dataArray) {
+  console.log('array passed: ', dataArray);
+  var query = connection.query('INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?)', [dataArray], (err, results) => {
+    if (err) throw err;
+    console.log('Data added: ', results);
+  });
+  console.log(query.sql);
+}
---
+++ bamazonCustomer.js
@@ -0,0 +1,74 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+require('console.table');
+
+var listOfItems = [];
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'bamazon'
+});
+
+function displayAll () {
+  conn.query('SELECT item_id, product_name, price FROM products', (err, result) => {
+    if (err) throw err;
+    listOfItems = [];
+    result.forEach(item => {
+      listOfItems.push(item.item_id);
+    });
+    console.table('Welcome to Bamazon!', result);
+    promptForPurchase();
+  });
+}
+
+displayAll();
+
+function promptForPurchase () {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'item',
+      message: 'Enter the item ID of the item you want to purchase:',
+      validate: function checkForNumber (id) {
+        return (listOfItems.indexOf(parseInt(id)) > -1);
+      }
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: 'Enter the number of quantity you want to purchase:',
+      validate: function checkForNumber (count) {
+        return Number.isInteger(parseInt(count));
+      }
+    }
+  ]).then(response => {
+    purchaseItem(parseInt(response.item), parseInt(response.quantity));
+  });
+}
+
+function purchaseItem (id, quantity) {
+  var query = conn.query('UPDATE products SET stock_quantity = stock_quantity - ' + quantity + ' WHERE ? AND ' + 'stock_quantity >' + quantity, [
+    {
+      item_id: id
+    }
+  ], function (err, res) {
+    if (err) throw err;
+
+
+    if (res.affectedRows > 0) {
+      conn.query(`SELECT price,product_name FROM products WHERE item_id = ${id}`, (err, res) =>{
+        if (err) throw err;
+        console.log(`You purchased ${quantity} of ${res[0].product_name} at $${res[0].price} for a total of $${res[0].price * quantity}`);
+        promptForPurchase();
+      })
+    } else {
+      conn.query(`SELECT stock_quantity,product_name FROM products WHERE item_id = ${id}`, (err, res) =>{
+        if (err) throw err;
+        console.log(`Sorry we don't have ${quantity} of ${res[0].product_name} in stock. We have only ${res[0].stock_quantity} in stock.`);
+        promptForPurchase();
+      })
+    }
+  });
+
+  console.log(query.html);
+}
